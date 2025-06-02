9f8400225c44d96cf9bade481c913ef318c1a450
luminousbinary <aolutolacaleb@gmail.com>
2025-06-02 10:16:47 +0100

    dates updated

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -54,8 +54,8 @@ const Index = () => {
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-24">
           <div className="text-center">
             <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
-              <span className="block text-lg">SINCE 2011</span>
-              <span className="block text-green-600">Your Healthy Fresh Alternative</span>
+              <span className="block text-green-600">Your Healthy Fresh Alternative
+                <br /> On The Go</span>
             </h1>
             <p className="mt-3 max-w-md mx-auto text-base text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl">
               WE DELIVER THE BEST PRODUCTS.
@@ -88,7 +88,7 @@ const Index = () => {
               Why Choose KRP Farm?
             </h2>
             <p className="mt-4 text-lg text-gray-500">
-             KRP Farms since 2012 has been delivering fruits, veggies & more across in Nigeria 
+             KRP Farms since 2011 has been delivering fruits, veggies & more across in Nigeria 
             </p>
           </div>
 
